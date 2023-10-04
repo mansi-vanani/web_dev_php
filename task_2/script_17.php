af15ba295b5a472ef4467bf8f645e26fde4c7f22
mansi-vanani <143687612+mansi-vanani@users.noreply.github.com>
2023-10-04 14:00:07 +0530

    Write a PHP script to find the largest of three numbers entered by the user.

---
+++ task_2/script_17.php
@@ -0,0 +1,16 @@
+<?php
+	$year=2023;
+	if($year%400==0)
+	{
+		echo $year . "is leap year";
+	}
+	elseif($year%4==0)
+	{
+		echo $year. "is leap year";
+	}
+	else
+	{
+		echo $year ."is not leap year";
+	}
+
+?>
